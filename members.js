bd8f40e4a89b4b7fa4b675a57123c04a46d021ab
Anjan Gorai <91726494+codecrazeAV@users.noreply.github.com>
2023-07-24 09:55:07 +0000

    final commit

---
+++ members.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    var skills = document.getElementById("skills").value;
+    var member = document.getElementById("member").value;
+    var memberName = document.getElementById("member").options[document.getElementById("member").selectedIndex].text;
+    var skillsList = skills.split(',');
+    var memberList = member.split(',');
+    var skillsListLength = skillsList.length;
+    var memberListLength = memberList.length;
+    var memberSkills = [];
+    var skillsMember = [];
+    var skillsMemberLength = [];
+    var skillsMemberLengthLength = 0;
+    var skillsMemberLengthLengthLength = 0;
+    var skillsMemberLengthLengthLengthLength = 0;
+    var skillsMemberLengthLengthLengthLengthLength = 0;
+    var skillsMemberLengthLengthLengthLengthLengthLength = 0;
+    var skillsMemberLengthLengthLengthLengthLengthLengthLength = 0;
+    var skillsMemberLengthLengthLengthLengthLengthLengthLengthLengthLength = 0;
+    var skillsMemberLengthLengthLengthLengthLengthLengthLengthLengthLengthLength = 0;
+    var skillsMemberLengthLength;
+}
